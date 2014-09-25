c5efdad64e1be1ccd442fa9ec092c73acced93c7
s10528 <yunron1020@gmail.com>
2014-09-26 07:23:31 +0900

    aaa

--- src/norites/Main.java
+++ src/norites/Main.java
@@ -1,9 +1,10 @@
 package norites;
 
+import java.util.Random;
+
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -15,8 +16,10 @@ import org.newdawn.slick.tiled.TiledMap;
 public class Main extends BasicGame {
 	/* 1. Main クラスまたはオブジェクトに所属するメンバー変数の宣言を書く所 */
 	float x = 180, y = 184;
+	float usax = 150, usay = 340;
 	int right = 1;
-
+	boolean usamuki= true;//trueだと右向き
+	Random rnd = new Random();
 	byte icount = 0;
 	boolean ismove = false;
 	static final float SPEED = 0.1f;
@@ -24,7 +27,7 @@ public class Main extends BasicGame {
 	private Animation noripie,walk,wait;
 	
 	String path =null;
-	Image unko, tinko;
+	Image unko, tinko, enemy,houdai,tama;
 	
 
 	TiledMap map = null;
@@ -56,15 +59,20 @@ public class Main extends BasicGame {
 		noripie = wait;
 		
 		try{
-			unko = new Image("./resource/map1.bmp");
+			unko = new Image("./resource/kabe1.png");
 
-			tinko = new Image("./resource/openspace2.bmp");
+			tinko = new Image("./resource/kabe2.png");
 			
+			enemy = new Image("./resource/usatan.gif");
+			
+			houdai = new Image("./resource/砲台1.gif");
+			
+			tama = new Image("./resource/砲弾.gif");
 		}catch(Exception e){
 		}
 		
 		path = "./resource/untitled.tmx";
-		System.out.println(path);
+		//System.out.println(path);
 		
 		try{
 			map = new TiledMap(path, false);
@@ -76,6 +84,7 @@ public class Main extends BasicGame {
 		map1 = map.getLayerIndex("タイル・レイヤー1");
 		map2 = map.getLayerIndex("collision");
 
+		
 	}
 	@SuppressWarnings("static-access")
 	@Override
@@ -103,15 +112,32 @@ public class Main extends BasicGame {
 		}
 		int ntx=(int)x/64; //のりぴーのタイル位置
 		int nty=(int)y/64; //のりぴーのタイル位置
-		System.out.println(ntx);
+		int utx=(int)usax/64;//うさたんのタイル位置
+		int uty=(int)usay/64;
 		
-		if(map.getTileId(ntx, nty, map2)==12 ||
-				map.getTileId(ntx+1, nty, map2)==12 || 
-				map.getTileId(ntx, nty+1, map2)==12 || 
-				map.getTileId(ntx+1, nty+1, map2) == 12){
+		if(map.getTileId(ntx, nty, map2)==3 ||
+				map.getTileId(ntx+1, nty, map2)==3 || 
+				map.getTileId(ntx, nty+1, map2)==3 || 
+				map.getTileId(ntx+1, nty+1, map2) == 3){
 			x=px;
 			y=py;
 		}
+		if(map.getTileId(ntx, nty, map1)==66){
+			x=77;
+			y=77;
+		}
+		if(ntx==utx && nty == uty){
+			for(;;){
+				x=rnd.nextInt(640-64);
+				y=rnd.nextInt(400-64);
+				if(map.getTileId((int)x/64, (int)y/64, map2)!=3 &&
+					map.getTileId((int)x/64+1, (int)y/64, map2)!=3 && 
+					map.getTileId((int)x/64, (int)y/64+1, map2)!=3 && 
+					map.getTileId((int)x/64+1, (int)y/64+1, map2) != 3)
+					break;
+			}
+		}
+		
 		
 		if (input.isKeyDown(input.KEY_LEFT)||
 			input.isKeyDown(input.KEY_RIGHT)||
@@ -133,25 +159,30 @@ public class Main extends BasicGame {
 		（ゲームの結果出力に関する本体・メインループ） */
 		
 		
-		for(int tx = 0; tx < 5; tx++){
-			for(int ty =0; ty<5;ty++){
-				//System.out.println(map.getTileId(tx,ty,map1));
-			if(map.getTileId(tx, ty, map1) == 1){
+		for(int tx = 0; tx < 10; tx++){
+			for(int ty =0; ty<7;ty++){
+			//	System.out.println(map.getTileId(tx,ty,map2));
+			if(map.getTileId(tx, ty, map1) == 2){
 				
 				
 				g.drawImage(unko,tx*64,ty*64);
 			}
-			if(map.getTileId(tx, ty, map1) == 2){
+			if(map.getTileId(tx, ty, map1) == 3){
 				
 				
 				g.drawImage(tinko,tx*64,ty*64);
 			}
-			//System.out.println(map.getTileId(tx, ty, map2));
-			if(map.getTileId(tx, ty, map2) == 12){
-			g.setColor(Color.green);
+			//System.out.println(map.getTileId(tx, ty, map1));
 			
-			//g.drawRect(tx*64,ty*64,64,64);
+			if(map.getTileId(tx, ty, map2) == 4){
+				g.drawImage(houdai,tx*64,ty*64);
 			}
+			
+			
+			
+			
+			
+			
 			
 			}
 	}
@@ -160,6 +191,19 @@ public class Main extends BasicGame {
 		}else if(right == -1){
 			noripie.draw((int)x+64,(int)y,right*64,64);
 		}
+		if(usamuki)
+			usax+=0.1;
+		
+		else
+			usax-=0.1;
+		//System.out.println(usamuki);
+		if(map.getTileId((int)usax/64, (int)usay/64, map2) == 3 && usamuki==false)
+			usamuki = true;
+		
+		else if(map.getTileId((int)usax/64+1, (int)usay/64, map2) == 3 && usamuki==true)
+			usamuki = false;
+		
+			g.drawImage(enemy, usax, usay);
 	}
 
 	public static void main(String[] args) throws SlickException {
@@ -167,7 +211,7 @@ public class Main extends BasicGame {
 		いわば着火メソッド。便宜上、このクラスに埋め込まれているだけで、
 		ゲームプログラム本体とは基本的に関係がない部分 */
 		AppGameContainer app = new AppGameContainer(new Main("骨組"));
-		app.setDisplayMode(640, 400, false);
+		app.setDisplayMode(680, 500, false);
 		app.start();
 	}
 }
