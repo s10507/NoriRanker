df5496bc1769eda3ad8e84e689b555f974e6226c
S10528 <S10528@410-61.salesio.salesio-sp.ac.jp>
2014-10-03 11:40:32 +0900

    しもびょんびょん

--- src/norites/Main.java
+++ src/norites/Main.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -15,6 +16,7 @@ import org.newdawn.slick.tiled.TiledMap;
 
 public class Main extends BasicGame {
 	/* 1. Main クラスまたはオブジェクトに所属するメンバー変数の宣言を書く所 */
+
 	float x = 180, y = 184;	
 	int ntx=(int)x/64; //のりぴーのタイル位置
 	int nty=(int)y/64; //のりぴーのタイル位置
@@ -23,8 +25,14 @@ public class Main extends BasicGame {
 	int utx=(int)usax/64;//うさたんのタイル位置
 	int uty=(int)usay/64;
 	
+	float shimo_x=200, shimo_y=200;
+	int stx=(int)shimo_x/64;//しもたんのタイル位置
+	int sty=(int)shimo_y/64;
+
+
 	int right = 1;
 	boolean usamuki= true;//trueだと右向き
+	boolean shimomuki =true; //trueだと下向き
 	Random rnd = new Random();
 	byte icount = 0;
 	boolean ismove = false;
@@ -33,7 +41,7 @@ public class Main extends BasicGame {
 	private Animation noripie,walk,wait;
 	
 	String path =null;
-	Image kabe1, kabe2, usatan,cannon,shell;
+	Image kabe1, kabe2, usatan,cannon,shell,shimo_normal,shimo_super;
 	
 
 	TiledMap map = null;
@@ -73,6 +81,10 @@ public class Main extends BasicGame {
 			cannon = new Image("./resource/砲台1.gif");
 			
 			shell = new Image("./resource/砲弾.gif");
+			
+			shimo_normal = new Image("./resource/シタ.gif");
+			
+			shimo_super = new Image("./resource/ウエ.gif");
 		}catch(Exception e){
 		}
 		
@@ -117,8 +129,7 @@ public class Main extends BasicGame {
 		}
 		int ntx=(int)x/64; //のりぴーのタイル位置
 		int nty=(int)y/64; //のりぴーのタイル位置
-		int utx=(int)usax/64;//うさたんのタイル位置
-		int uty=(int)usay/64;
+
 		
 		if(map.getTileId(ntx, nty, map2)==3 ||
 				map.getTileId(ntx+1, nty, map2)==3 || 
@@ -131,7 +142,7 @@ public class Main extends BasicGame {
 			x=77;
 			y=77;
 		}
-		if(ntx==utx && nty == uty){
+		if(((int)x+32)/64==(((int)usax+32)/64) && ((int)y+32)/64 == ((int)usay+32)/64){
 			for(;;){
 				x=rnd.nextInt(640-64);
 				y=rnd.nextInt(400-64);
@@ -201,7 +212,49 @@ public class Main extends BasicGame {
 		else if(map.getTileId((int)usax/64+1, (int)usay/64, map2) == 3 && usamuki==true)
 			usamuki = false;
 		
+		if(map.getTileId((int)usax/64, (int)usay/64+1, map2) == 3 && usamuki==false)
+			usamuki = true;
+		
+		else if(map.getTileId((int)usax/64+1, (int)usay/64+1, map2) == 3 && usamuki==true)
+			usamuki = false;
 			g.drawImage(usatan, usax, usay);
+
+			
+			
+			
+			
+			if(shimomuki){
+				g.drawImage(shimo_normal, shimo_x,shimo_y);
+				shimo_y+=0.1;
+			
+			}else{
+				shimo_y-=0.1;
+				shimo_y-=0.1;
+				g.drawImage(shimo_super, shimo_x,shimo_y);
+			}
+			if(map.getTileId((int)shimo_x/64, (int)shimo_y/64, map2) == 3 && shimomuki==false){
+				shimomuki = true;
+				g.drawImage(shimo_normal, shimo_x,shimo_y);
+			}else if(map.getTileId((int)shimo_x/64+1, (int)shimo_y/64, map2) == 3 && shimomuki==true){
+				shimomuki = false;
+				g.drawImage(shimo_super, shimo_x,shimo_y);
+				}
+			if(map.getTileId((int)shimo_x/64, (int)shimo_y/64+1, map2) == 3 && shimomuki==false){
+				shimomuki = true;
+				g.drawImage(shimo_normal, shimo_x,shimo_y);
+			}else if(map.getTileId((int)shimo_x/64+1, (int)shimo_y/64+1, map2) == 3 && shimomuki==true){
+				shimomuki = false;
+				g.drawImage(shimo_super, shimo_x,shimo_y);
+			}
+			
+			
+			g.setColor(Color.blue);
+			g.drawRect(x, y, 64, 64);
+			g.drawRect(usax, usay, 64, 64);
+			g.drawRect(shimo_x, shimo_y, 64, 64);
+			System.out.println("noriko"+(int)x+":"+(int)y);
+			System.out.println("usagi"+(int)usax+":"+(int)usay);
+
 	}
 
 	public static void main(String[] args) throws SlickException {
