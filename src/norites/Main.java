32d4b35fb7bc834564457a5f97d43043a9b54a41
s10528 <yunron1020@gmail.com>
2014-10-31 12:23:09 +0900

    砲弾のりぴーサーチ

--- src/norites/Main.java
+++ src/norites/Main.java
@@ -17,30 +17,30 @@ import org.newdawn.slick.tiled.TiledMap;
 
 public class Main extends BasicGame {
 	/* 1. Main クラスまたはオブジェクトに所属するメンバー変数の宣言を書く所 */
-	
+
 	final int WARP_ID = 0;
-	final int WALL1_ID = 1;	
+	final int WALL1_ID = 1;
 	final int WALL2_ID = 2;
 	final int FLOOR = 4;
 	final int CEILING = 5;
 	final int CANNON_ID = 3;
 	final int TAKARA_ID = 6;
-	
-	float x = 65, y = 65;	
+
+	float x = 65, y = 65;
 //	int ntx=(int)x/64; //のりぴーのタイル位置
 //	int nty=(int)y/64; //のりぴーのタイル位置
-		
+
 	float usax = 150, usay = 300;
 	int utx=(int)usax/64;//うさたんのタイル位置
 	int uty=(int)usay/64;
-	
+
 	float shimo_x=200, shimo_y=200;
 	int stx=(int)shimo_x/64;//しもたんのタイル位置
 	int sty=(int)shimo_y/64;
 
 	int menu_id =0;
 
-	
+
 	int right = 1;
 	boolean usamuki= true;//trueだと右向き
 	boolean shimomuki =true; //trueだと下向き
@@ -50,10 +50,10 @@ public class Main extends BasicGame {
 	static final float SPEED = 0.1f;
 	Image[] sprite = new Image[7];
 	private Animation noripie,walk,wait;
-	
+
 	String path =null;
 	Image kabe1, kabe2, usatan,cannon,shell,shimo_normal,shimo_super, takara;
-	
+
 	ArrayList<Integer> cannon_x_list = new ArrayList<>();
 	ArrayList<Integer> cannon_y_list = new ArrayList<>();
 	int cannon_number = 0;
@@ -61,12 +61,12 @@ public class Main extends BasicGame {
 	int shell_x = 0;
 
 	TiledMap map = null;
-	
-	
+
 	int map1, map2, map3;
-	
+
 	boolean onground;
-	
+	int next_cannon;
+
 	public Main(String title) {
 		super(title);
 	}
@@ -91,76 +91,76 @@ public class Main extends BasicGame {
 		wait = new Animation(waiting, duration, true);
 
 		noripie = wait;
-		
+
 		try{
 			kabe1 = new Image("./resource/kabe1.png");
 
 			kabe2 = new Image("./resource/kabe2.png");
-			
+
 			usatan = new Image("./resource/usatan.gif");
-			
+
 			cannon = new Image("./resource/砲台1.gif");
-			
+
 			shell = new Image("./resource/砲弾.gif");
-			
+
 			shimo_normal = new Image("./resource/シタ.gif");
-			
+
 			shimo_super = new Image("./resource/ウエ.gif");
-			
+
 			takara = new Image("./resource/takara.gif");
-			
+
 		}catch(Exception e){
 		}
-		
+
 		path = "./resource/untitled.tmx";
 		//System.out.println(path);
-		
+
 		try{
 			map = new TiledMap(path, false);
 		}catch(SlickException e){
-			System.out.println("Error Loading Map!");		
-		}	
-				
+			System.out.println("Error Loading Map!");
+		}
+
 		map1 = map.getLayerIndex("タイル・レイヤー1");
 		map2 = map.getLayerIndex("collision");
 		map3 = map.getLayerIndex("ceilingandfloor");
 		try{
-			//System.out.println(map.getTileProperty(3, "number", "true"));			
+			//System.out.println(map.getTileProperty(3, "number", "true"));
 		}catch(NullPointerException e){
 			System.err.println(e.getMessage());
-		}		
+		}
 //		System.out.println("map1: "+map1+" map2: "+map2);
-							
-		for(int tx = 0;tx < 10; tx++){
-			for(int ty = 0;ty < 7; ty++){
+
+		for(int ty = 0;ty < 7; ty++){
+			for(int tx = 0;tx < 10; tx++){
 				if(map.getTileId(tx, ty, map2) == CANNON_ID){
 					cannon_x_list.add(tx*64);
 					cannon_y_list.add(ty*64);
 				}
 			}
-		}		
-		
+		}
+
 		for(int tx = 0;tx < cannon_x_list.size(); tx++){
 //			System.out.println("cannon_x_list: "+ cannon_x_list.get(tx));
 		}
 		for(int ty = 0;ty < cannon_y_list.size(); ty++){
 //			System.out.println("cannon_y_list: "+ cannon_y_list.get(ty));
-		}		
+		}
 	}
 	@SuppressWarnings("static-access")
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
 		/* 4. ゲームの内部状態（変数等）の更新に関するルーチン
-		 * 
+		 *
 		（ゲームのロジックや入力に関する本体・メインループ） */
 		float px=x,py=y;
-		
+
 		float move = SPEED * delta;
 		Input input = gc.getInput();
 		if (input.isKeyDown(input.KEY_LEFT)) {
 			x -= move;
 			right = -1;
-			
+
 		} else if (input.isKeyDown(input.KEY_RIGHT)) {
 			x += move;
 			right = 1;
@@ -169,7 +169,7 @@ public class Main extends BasicGame {
 			y -= move;
 		} else if (input.isKeyDown(input.KEY_DOWN)) {
 			y += move;
-		}		
+		}
 		if(input.isKeyDown(input.KEY_1)){
 			cannon_number = 0;
 			shell_x = cannon_x_list.get(cannon_number)-64;
@@ -182,11 +182,7 @@ public class Main extends BasicGame {
 			cannon_number = 2;
 			shell_x = cannon_x_list.get(cannon_number)-64;
 		}
-		if(input.isKeyDown(input.KEY_4)){
-			cannon_number = 3;
-			shell_x = cannon_x_list.get(cannon_number)-64;
-		}
-				
+
 		if(map.getTileId((int)(x+50)/64, (int)(y+50)/64, map2)==WALL2_ID ||		//のりぴーの右下と壁判定
 				map.getTileId((int)(x+50)/64, (int)(y+10)/64, map2)==WALL2_ID || 		//のりぴーの右上と壁判定
 				map.getTileId((int)(x+10)/64, (int)(y+50)/64, map2)==WALL2_ID || 			//のりぴーの左下と壁判定
@@ -196,52 +192,52 @@ public class Main extends BasicGame {
 				map.getTileId((int)(x+10)/64, (int)(y+50)/64, map2)==CANNON_ID || 			//キャノンの左下と壁判定
 				map.getTileId((int)(x+10)/64, (int)(y+10)/64, map2) == CANNON_ID){				//キャノンの左上と壁判定
 			x=px;			//前の位置にもどす
-			y=py;							
+			y=py;
 		}
-		
+
 		if(map.getTileId((int)(x+32)/64, (int)(y+51)/64, map3)==FLOOR)
 			onground = true;
 		else
 			onground = false;
-		
-		System.out.println("x:"+(int)(x+50)+" "+"y:"+(int)(y+50)+" "+onground);
-		
+
+//		System.out.println("x:"+(int)(x+50)+" "+"y:"+(int)(y+50)+" "+onground);
+
 //		System.out.println(onground);
-		
+
 		if(map.getTileId((int)(x+50)/64 ,(int)(y+10)/64,map1) == WARP_ID ||
 		map.getTileId((int)(x+10)/64 ,(int)(y+10)/64,map1) == WARP_ID ){		//落下ワープとのりぴーの判定
 			x=100;y=100;
 		}
-	
-			
+
+
 //		System.out.print("50 : ("+(x+50)/64+", "+(y+50)/64+")\n");
 //		System.out.print("10 : ("+(x+10)/64+", "+(y+10)/64+")\n");
 		if(map.getTileId((int)(x+10)/64, (int)(y+10)/64, map2) == TAKARA_ID){
 			menu_id=2;
 		}
-		
-		if(((int)x+32)/64==(((int)usax+32)/64) && ((int)y+32)/64 == ((int)usay+32)/64 || 
-				((int)x+32)/64==(((int)shimo_x+32)/64) && ((int)y+32)/64 == ((int)shimo_y+32)/64|| 
+
+		if(((int)x+32)/64==(((int)usax+32)/64) && ((int)y+32)/64 == ((int)usay+32)/64 ||
+				((int)x+32)/64==(((int)shimo_x+32)/64) && ((int)y+32)/64 == ((int)shimo_y+32)/64||
 				((int)x+32)/64==(((int)shell_x+32)/64) && ((int)y+32)/64 == ((int)cannon_y_list.get(cannon_number)+32)/64){	//障害物たちのあたり判定
 			for(;;){
 				x=rnd.nextInt(map.getWidth()*64-64);	//当たったらランダムにふっとばす
 				y=rnd.nextInt(map.getHeight()*64-64);
 				if(map.getTileId((int)(x+50)/64, (int)(y+50)/64, map2)!=WALL2_ID &&		//ランダムで吹っ飛んだ先が壁や障害物にならないように
-						map.getTileId((int)(x+50)/64, (int)(y+10)/64, map2)!=WALL2_ID && 
-						map.getTileId((int)(x+10)/64, (int)(y+50)/64, map2)!=WALL2_ID && 
+						map.getTileId((int)(x+50)/64, (int)(y+10)/64, map2)!=WALL2_ID &&
+						map.getTileId((int)(x+10)/64, (int)(y+50)/64, map2)!=WALL2_ID &&
 						map.getTileId((int)(x+10)/64, (int)(y+10)/64, map2)!=WALL2_ID &&
 						map.getTileId((int)(x+50)/64, (int)(y+50)/64, map2)!=CANNON_ID &&
-						map.getTileId((int)(x+50)/64, (int)(y+10)/64, map2)!=CANNON_ID && 
-						map.getTileId((int)(x+10)/64, (int)(y+50)/64, map2)!=CANNON_ID && 
+						map.getTileId((int)(x+50)/64, (int)(y+10)/64, map2)!=CANNON_ID &&
+						map.getTileId((int)(x+10)/64, (int)(y+50)/64, map2)!=CANNON_ID &&
 						map.getTileId((int)(x+10)/64, (int)(y+10)/64, map2)!=CANNON_ID &&
-						map.getTileId((int)(x+50)/64, (int)(y+10)/64, map1)!=WARP_ID && 
-						map.getTileId((int)(x+10)/64, (int)(y+50)/64, map1)!=WARP_ID && 
+						map.getTileId((int)(x+50)/64, (int)(y+10)/64, map1)!=WARP_ID &&
+						map.getTileId((int)(x+10)/64, (int)(y+50)/64, map1)!=WARP_ID &&
 						map.getTileId((int)(x+10)/64, (int)(y+10)/64, map1)!=WARP_ID )
 					break;
 			}
 		}
-		
-		
+
+
 		if (input.isKeyDown(input.KEY_LEFT)||
 			input.isKeyDown(input.KEY_RIGHT)||
 			input.isKeyDown(input.KEY_UP)||
@@ -255,6 +251,7 @@ public class Main extends BasicGame {
 			noripie = wait;
 			noripie.update(delta);
 		};
+		
 	}
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException {
@@ -262,7 +259,7 @@ public class Main extends BasicGame {
 		（ゲームの結果出力に関する本体・メインループ） */
 		Input input = gc.getInput();
 		if(menu_id==0){			//メニュー画面の起動
-			
+
 			g.setColor(Color.magenta);
 			g.drawString("start!!!!!!!!!!!!!!!!!!!!", 100, 200);	//メニュー内の文字
 			if(input.isKeyDown(input.KEY_A)){						//裏ワザ←ただの脱線
@@ -272,25 +269,23 @@ public class Main extends BasicGame {
 			if(input.isKeyDown(input.KEY_ENTER)){					//enter押すとゲームスタート
 				menu_id=1;
 			}
-			
-			
 		}else{
-		
+
 		for(int tx = 0; tx < 10; tx++){							//マップ描画
 			for(int ty =0; ty<7;ty++){
 //				System.out.println(map.getTileId(tx,ty,map3));
-				if(map.getTileId(tx, ty, map1) == WALL1_ID){			//背景の壁						
+				if(map.getTileId(tx, ty, map1) == WALL1_ID){			//背景の壁
 					g.drawImage(kabe1,tx*64,ty*64);
 				}
 				if(map.getTileId(tx, ty, map1) == WALL2_ID){			//存在する壁
 					g.drawImage(kabe2,tx*64,ty*64);
 				}
 				//System.out.println(map.getTileId(tx, ty, map1));
-				
+
 				if(map.getTileId(tx, ty, map2) == CANNON_ID){			//キャノン
 					g.drawImage(cannon,tx*64,ty*64);
-				}	
-				
+				}
+
 				if(map.getTileId(tx, ty, map2)==TAKARA_ID){
 					g.drawImage(takara,tx*64,ty*64);
 				}
@@ -310,17 +305,17 @@ public class Main extends BasicGame {
 		//System.out.println(usamuki);
 		if(map.getTileId((int)usax/64, (int)usay/64, map2) == WALL2_ID && usamuki==false)
 			usamuki = true;
-		
+
 		else if(map.getTileId((int)usax/64+1, (int)usay/64, map2) == WALL2_ID && usamuki==true)
 			usamuki = false;
-		
+
 		if(map.getTileId((int)usax/64, (int)usay/64+1, map2) == WALL2_ID && usamuki==false)
 			usamuki = true;
-		
+
 		else if(map.getTileId((int)usax/64+1, (int)usay/64+1, map2) == WALL2_ID && usamuki==true)
 			usamuki = false;
 			g.drawImage(usatan, usax, usay);
-			
+
 		if(shimomuki){
 			g.drawImage(shimo_normal, shimo_x,shimo_y);
 			shimo_y+=0.1;
@@ -343,15 +338,20 @@ public class Main extends BasicGame {
 			shimomuki = false;
 			g.drawImage(shimo_super, shimo_x,shimo_y);
 		}
-			
-		shell_x--;
 		
-		if(shell_x<=64)
-			shell_x = cannon_x_list.get(cannon_number)-64;
-								
+
+		shell_x--;
+		if(shell_x<=64){
+			for (int i = 0;i < cannon_y_list.size(); i++)
+				if(cannon_y_list.get(i)/64 == (int)(y+50)/64){
+					System.out.println("a"+i);
+					cannon_number = i;
+				}
+			shell_x = cannon_x_list.get(cannon_number)-64;						
+		}
 		if(shell_flag)
 			g.drawImage(shell, shell_x, cannon_y_list.get(cannon_number));
-				           
+
 		g.setColor(Color.blue);
 		g.drawRect(x, y, 64, 64);
 		g.drawRect(usax, usay, 64, 64);
@@ -361,11 +361,11 @@ public class Main extends BasicGame {
 		g.drawRect((((int)x+50)/64)*64, (((int)y+10)/64)*64, 5, 5);
 		g.drawRect((((int)x+10)/64)*64, (((int)y+50)/64)*64, 5, 5);
 		g.drawRect((((int)x+10)/64)*64, (((int)y+10)/64)*64, 5, 5);
-		
+
 //			System.out.println("noriko"+(int)x+":"+(int)y);
 //			System.out.println("usagi"+(int)usax+":"+(int)usay);
 	}
-	
+
 	}
 	public static void main(String[] args) throws SlickException {
 		/* 6. JVM 側がこの Main クラスを実体化するための、
@@ -375,7 +375,7 @@ public class Main extends BasicGame {
 		AppGameContainer app = new AppGameContainer(new Main("骨組"));
 		app.setDisplayMode(64*10, 64*7, false);
 		app.start();
-		
-		
+
+
 	}
 }
